649f6b353a5ff7a39dfeeefadc44349ad3541716
¨Alex-Bc191¨ <202400892j@utea.edu.pe>
2025-05-26 09:36:27 -0500

    :v

---
+++ VariablesEnteras.java
@@ -0,0 +1,12 @@
+public class VariablesEnteras {
+    public static void main(String[] args) {
+        //Programa que muestra una variable entera
+        int numero;
+        numero = 5;
+        System.out.println("El valor de mi variable es " + numero);
+        numero = 20;
+        System.out.println("y ahora es " + numero);
+
+    }
+
+}
